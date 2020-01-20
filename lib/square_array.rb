1d2166ce25a375917decb17fd76f1804edbe67f7
calm-fork-7535 <komald.4@gmail.com>
2020-01-20 19:53:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,20 +1,6 @@
 #use while loop
 #square each number and return a new array with squared numbers
 
-
-
-# def square_array(array)
-#   new_numbers = []
-#   numbers = 0
-#   #while array[numbers] do
-#     while numbers < array.length do
-#       new_numbers << array[numbers] ** 2
-#     numbers += 1
-#   end
-# return new_numbers
-# end
-
-
 def square_array(array)
   new_numbers = []
   numbers = 0
